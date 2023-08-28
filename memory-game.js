e11001d7dedfc979397f17c5fb61e7756caa913f
Mohammad Rayhan Hosain <67748217+rayhanhosain@users.noreply.github.com>
2023-08-28 16:25:34 +0600

    Update memory-game.js

--- memory-game.js
+++ memory-game.js
@@ -49,19 +49,29 @@ if (matchNumCount === 10) {
 
     let arrOfImgNum = [];
 // it's not understandable, what you meant to achieve in this game. so comment purpose of functions here, so that it can be understood, and a readme file.
-   //images object will be looped through one by one and for each object, one img tag will be made where the images will be put serially after the random sort done previously
+//images object will be looped through one by one and for each object, one img tag will be made where the images will be put serially after the random sort done previously
     images.forEach((obj, imgNum) => {
       const singleImage = document.createElement("img");
       singleImage.src = "./image/question-mark.jpg";
       singleImage.dataset.imgNum = imgNum;
       let singleImageField = whereImgs.appendChild(singleImage);
+
+      //checks if the clicksLeftCount is greater than 0. If it is greater than 0, click event listener is added in each photo. After each click, clickLeftCount is decreased by one.
       if (clicksLeftCount > 0) {
         singleImageField.addEventListener("click", () => {
           clicksLeftCount--;
           clicksLeft.innerText = clicksLeftCount;
+
+          //after click on a particular image, this condition checks if arrOfImg's length is 0. 
+          //If so, the that picture gets changed by according to the index number and pushes the index number to the arrOfImg number.
           if (arrOfImgNum.length === 0) {
             singleImage.src = images[imgNum].imgLink;
             arrOfImgNum.push(imgNum);
+
+            //this checks whether the length of arrOfImgNumber is 1 and the index of the clicked image 
+            //is the same as that of the previous click. That means, this whether one previous click was 
+            //made and whther it is not the same image that was clicked before. If one previous click 
+            //was made and it is the not the same indexed field as before, this condition applies
           } else if (arrOfImgNum.length === 1 && arrOfImgNum[0] !== imgNum) {
             singleImage.src = images[imgNum].imgLink;
             arrOfImgNum.push(imgNum);
